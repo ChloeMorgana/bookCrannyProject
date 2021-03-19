06bc7f132c476fa136e34a2a256f5cef517478e3
Chloe Smart <2439047s@student.gla.ac.uk>
2021-03-19 14:11:50 +0000

    Models updated

--- bookCranny/models.py
+++ bookCranny/models.py
@@ -1,13 +1,37 @@
-### DUMMY CODE TO PREVENT ERRORS IN IMPORTS
-### FEEL FREE TO DELETE EVERYTHING BELOW
 from django.db import models
 from django.contrib.auth.models import User
 
 class User(models.Model):
-    True
+    username= models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
+
 class Book(models.Model):
-    True
-class Review(models.Model):
-    True
+    ISBN= models.CharField(max_length=13, unique=True)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    blurb = models.CharField(max_length=1000)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Rating(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    ISBN = models.ForeignKey(Book, on_delete=models.CASCADE)
+    review = models.CharField(max_length=1000)
+    stars = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.stars
+
+class Genre(models.Model):
+    name= models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Wishlist(models.Model):
-    True
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
