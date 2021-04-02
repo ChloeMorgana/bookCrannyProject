d18fcd39868480e9d2cd931571daf48c775fa656
Rory Graham <2460896G@student.gla.ac.uk>
2021-04-02 16:10:04 +0100

    added admin.py so database can be managed from admin interface

---
+++ bookCranny/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from bookCranny.models import Genre, Wishlist, Book, Rating
+
+admin.site.register(Genre)
+admin.site.register(Wishlist)
+admin.site.register(Book)
+admin.site.register(Rating)
