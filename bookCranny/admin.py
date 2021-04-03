49fb803a8e468f71d9f4564968b85edf884dc546
Forbes Lai <2445570L@student.gla.ac.uk>
2021-04-03 23:39:25 +0800

    update admin

--- bookCranny/admin.py
+++ bookCranny/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from bookCranny.models import Genre, Wishlist, Book, Rating
+from bookCranny.models import Wishlist, Book, Rating, Genre
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('ISBN', 'title', 'author', 'genre')
+
+admin.site.register(Book, BookAdmin)
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('ISBN', 'username', 'stars', 'review')
+
+admin.site.register(Rating, RatingAdmin)
 
 admin.site.register(Genre)
-admin.site.register(Wishlist)
-admin.site.register(Book)
-admin.site.register(Rating)
