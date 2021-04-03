6fb738230181d07de6a019281e0c19c546957480
Chloe Smart <2439047s@student.gla.ac.uk>
2021-04-03 10:28:52 +0100

    added population json file

--- bookCranny/models.py
+++ bookCranny/models.py
@@ -1,26 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Genre(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
     def __str__(self):
         return self.name
 
-class Wishlist(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    time = models.DateTimeField(auto_now_add=True)
-
 class Book(models.Model):
     ISBN = models.CharField(max_length=13, unique=True)
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
-    blurb = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-    wishlist = models.ManyToManyField(Wishlist)
 
     def __str__(self):
         return self.title
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    books = models.ManyToManyField(Book)
+    time = models.DateTimeField(auto_now_add=True)
 
 class Rating(models.Model):
     ratingID = models.AutoField(primary_key=True)
