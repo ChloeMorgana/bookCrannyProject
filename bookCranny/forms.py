ba2ba6da4bf1c30c16a12ed77574150cf84a92ba
Forbes Lai <2445570L@student.gla.ac.uk>
2021-04-05 05:21:34 +0800

    typo

--- bookCranny/forms.py
+++ bookCranny/forms.py
@@ -17,7 +17,7 @@ class BookForm(forms.ModelForm):
     ISBN = forms.CharField(max_length=13, help_text = "Unique identifier of the book (10/13 digits long)")
     title = forms.CharField(max_length=100, help_text = "Title of the book")
     author = forms.CharField(max_length=50, help_text = "Author of the book")
-    description = forms.CharField(widget = forms.Textarea, max_length=1000, help_text = "Enter a short description for the book (maximum 1000 characters", required = False)
+    description = forms.CharField(widget = forms.Textarea, max_length=1000, help_text = "Enter a short description for the book (maximum 1000 characters)", required = False)
     genre = forms.ModelChoiceField(queryset = GENRES)
     
     
