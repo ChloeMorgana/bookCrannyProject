7ff6caf661e3f2dfaaf47313f7b1dbba6ce4aec2
Rory Graham <2460896G@student.gla.ac.uk>
2021-04-05 14:11:26 +0100

    improved styling in register.html and newbook.html

--- bookCranny/forms.py
+++ bookCranny/forms.py
@@ -14,10 +14,11 @@ class UserForm(UserCreationForm):
 
 class BookForm(forms.ModelForm):
     GENRES = Genre.objects.all()
-    ISBN = forms.CharField(max_length=13, help_text = "Unique identifier of the book (10/13 digits long)")
-    title = forms.CharField(max_length=100, help_text = "Title of the book")
-    author = forms.CharField(max_length=50, help_text = "Author of the book")
-    description = forms.CharField(widget = forms.Textarea, max_length=1000, help_text = "Enter a short description for the book (maximum 1000 characters)", required = False)
+    ISBN = forms.CharField(max_length=13)
+    ISBN.label = "ISBN"
+    title = forms.CharField(max_length=100)
+    author = forms.CharField(max_length=50)
+    description = forms.CharField(widget = forms.Textarea, max_length=1000, required = False)
     genre = forms.ModelChoiceField(queryset = GENRES)
     
     def clean(self):
